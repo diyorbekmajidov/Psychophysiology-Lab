4a4d4193e28b87242156c2da871d1ed8d46b76cb
Diyorbek <majidovdiyorbek741@gmail.com>
2026-06-03 15:05:31 +0500

    secret password

--- config/settings.py
+++ config/settings.py
@@ -66,10 +66,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME', default='psychology_lab'),
+        'USER': env('DB_USER', default='postgres'),
+        'PASSWORD': env('DB_PASSWORD', default='postgres'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
